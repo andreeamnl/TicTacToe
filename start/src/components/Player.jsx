174e4a5cace8e5a98fe03d48d88a05a49cb0f10c
OzzyOsbourne <andreeamanole2020@gmail.com>
2024-08-15 19:55:50 +0300

    User Input

--- start/src/components/Player.jsx
+++ start/src/components/Player.jsx
@@ -2,14 +2,19 @@ import { useState } from "react"
 
 
 
-export default function Player({name, symbol}){
+export default function Player({initialName, symbol}){
+    const [name, setPlayerName] = useState(initialName)
     const [isEditing, SetisEditing] = useState(false);
     let name_tab = ''
+
+    function handleChange(event){
+        setPlayerName(event.target.value)
+    }
 
     if(!isEditing)
         {name_tab=   <span className="player-name">{name}</span>}
     else
-        { name_tab = <input type="text" required value={name}/>}
+        { name_tab = <input type="text" required defaultValue={name} onChange={handleChange}/>}
 
     return(
         <li>
